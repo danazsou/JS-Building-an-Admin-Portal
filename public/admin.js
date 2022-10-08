a795b170f3ca5e7c433d36ba07b7fed26e0ca951
danazou <dana.sou@gmail.com>
2022-10-07 19:11:14 -0700

    committing for first save with a note

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,13 @@
 
 // Your Code Here
+
+//make sure to add async along with await so it won't be a long response time
+ function admin(){
+    
+    const bookListResponse = fetch('http://localhost:3001/ListBooks');
+    const bookList = bookListResponse.json();
+
+
+
+    const ul = document.createElement('ul');
+}
