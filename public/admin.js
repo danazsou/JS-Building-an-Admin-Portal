f8fa9c4be91f09dada879b92ad43c94a1c5b3c8a
danazou <dana.sou@gmail.com>
2022-10-10 23:22:19 -0700

    final commit

--- public/admin.js
+++ public/admin.js
@@ -2,12 +2,43 @@
 // Your Code Here
 
 //make sure to add async along with await so it won't be a long response time
- function admin(){
+async function main(){
+    let response = await fetch('http://localhost:3001/listBooks')
+ 
+    let books = await response.json()
     
-    const bookListResponse = fetch('http://localhost:3001/ListBooks');
-    const bookList = bookListResponse.json();
+    books.forEach(displayBook)
+ 
+ }
+ function displayBook(book){
+     
+    let root = document.querySelector('#root')
+ 
+     let li = document.createElement('li')
+     li.textContent = book.title
+ 
+     let input = document.createElement('input')
+     input.value = book.quantity
+ 
+     let saveButton = document.createElement('button')
+     saveButton.textContent= 'Save'
+ 
+     saveButton.addEventListener('click', function(){
+         fetch('http://localhost:3001/updateBook',{
+                    
+             method: 'PATCH',
+             headers: { 'Content-Type': 'application/json'},
+             body: JSON.stringify({
+                 id: book.id,
+                 quantity: input.value
+             })
+         })
+     })
+ 
+     li.append(input, saveButton)
+     root.append(li)
+ }
+ 
+ main()
 
-
-
-    const ul = document.createElement('ul');
-}
+ 
